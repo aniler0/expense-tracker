57fa2760dd874164eba6f6862efd45e586b1af58
aniler0 <anil.er@hotmail.com>
2021-10-16 14:48:21 +0300

    redux and userType definition are ready

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import ReactDOM from "react-dom";
-import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 import "antd/dist/antd.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./App";
+import rootReducers from "./store";
+
+const store = createStore(rootReducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

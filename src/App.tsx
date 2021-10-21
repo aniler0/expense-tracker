53a7400c5d37e2efcdf902095f9dc6a441ee96b3
aniler0 <anil.er@hotmail.com>
2021-10-21 23:10:47 +0300

    user register page is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import React from 'react'
+import { Route } from 'react-router'
+import SignUp from './components/SignUp'
+import { Layout, Menu } from 'antd'
+
+const { Header, Content, Footer } = Layout
 function App() {
-  return <>Expense Tracker</>;
+	return (
+		<Layout>
+			<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+				<div className='logo' />
+				<Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']}>
+					<Menu.Item key='1'>nav 1</Menu.Item>
+					<Menu.Item key='2'>nav 2</Menu.Item>
+					<Menu.Item key='3'>nav 3</Menu.Item>
+				</Menu>
+			</Header>
+			<Content className='site-layout' style={{ padding: '50px', marginTop: 64 }}>
+				<Route path='/register' component={SignUp} />
+			</Content>
+			<Footer style={{ textAlign: 'center' }}>Expense Tracker @ Anıl Er</Footer>
+		</Layout>
+	)
 }
 
-export default App;
+export default App

7ce39aae527f59e259c4d857f5643e1215d4b6c1
aniler0 <anil.er@hotmail.com>
2021-10-23 15:20:49 +0300

    private route is created

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Redirect, Route, RouteProps } from "react-router";
+
+interface PrivateRouteProps extends RouteProps {
+  component: React.FC<any>;
+}
+
+const PrivateRoute = ({
+  component: Component,
+  ...theRest
+}: PrivateRouteProps) => {
+  return (
+    <Route
+      {...theRest}
+      render={(props) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+          return <Component {...props} />;
+        }
+        return <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
